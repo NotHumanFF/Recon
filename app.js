43cc13ad27e865456eb09339e589cc7b6ee97731
Parth Awasthi <107567458+NotHumanFF@users.noreply.github.com>
2023-01-10 20:06:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash")
+
+
+const app = express();
+app.set('view engine' , 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+let startingtitle = "";
+
+
+app.get("/" , function (req,res) {
+    res.render("home" , {startingtitle: "Home"});
+})
+
+app.get("/contact" , function (req,res) {
+    res.render("contact" , {startingtitle: "Contact"});
+})
+
+app.get("about" , function (req,res) {
+    res.render("about");
+})
+
+app.get("/playlist" , function (req,res) {
+    res.render("playlist");
+
+})
+
+
+app.listen("5000" , function () {
+    console.log("server started running on port 5000");
+})
+
