fad15bf83fd1a0221e517c9476d2c2007f0ab9ca
Parth Awasthi <107567458+NotHumanFF@users.noreply.github.com>
2023-01-26 00:46:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,6 +30,38 @@ app.get("/playlist" , function (req,res) {
 })
 
 
+
+
+// const app = express();
+// app.set('view engine' , 'ejs');
+// app.use(bodyParser.urlencoded({extended:true}));
+// app.use(express.static("public"));
+
+// let startingtitle = "";
+
+
+// app.get("/" , function (req,res) {
+//     res.render("home" , {startingtitle: "Home"});
+// })
+
+// app.get("/contact" , function (req,res) {
+//     res.render("contact" , {startingtitle: "Contact"});
+// })
+
+// app.get("about" , function (req,res) {
+//     res.render("about");
+// })
+
+// app.get("/playlist" , function (req,res) {
+//     res.render("playlist");
+
+// })
+
+
+
+
+
+
 app.listen("5000" , function () {
     console.log("server started running on port 5000");
 })
