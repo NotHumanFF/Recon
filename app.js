0127f1a6ed6230f6b7f813712ad37046d554c5bd
Parth Awasthi <107567458+NotHumanFF@users.noreply.github.com>
2023-12-06 00:34:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash")
 
-
 const app = express();
 app.set('view engine' , 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
